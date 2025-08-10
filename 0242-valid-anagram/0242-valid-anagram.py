adf1ab29cfd156929e10396fb300e0dff5efba5e
ainas <ainasrhmn@gmail.com>
2025-08-10 22:24:53 +0530

    Time: 0 ms (100%), Space: 12.5 MB (82.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,15 @@
 class Solution(object):
     def isAnagram(self, s, t):
-       return  "".join(sorted(s)) == "".join(sorted(t))
+       
+        if len(s) != len(t):
+            return False
+
+        # Take unique characters from s
+        char = set(s)
+
+        # iterate through that uniue characters
+        for c in char:
+            #  Check two Character count are same then if not it not a anagram word
+            if s.count(c) != t.count(c):
+                return False
+        return True
