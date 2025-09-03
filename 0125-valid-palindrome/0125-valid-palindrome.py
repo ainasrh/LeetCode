2d1e478d80d0a3d409efd02fb1c4f7d70180a7ce
ainas <ainasrhmn@gmail.com>
2025-09-03 21:49:13 +0530

    Time: 23 ms (35.96%), Space: 12.7 MB (79.15%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,4 +1,28 @@
 class Solution(object):
     def isPalindrome(self, s):
-        alpha_ = "".join(char.lower() for char in s if char.isalpha() or char.isdigit())
-        return alpha_ == alpha_[::-1]
+        # alpha_ = "".join(char.lower() for char in s if char.isalpha() or char.isdigit())
+        # return alpha_ == alpha_[::-1]
+
+        # Two pointers
+
+        l = 0
+        r = len(s) - 1
+        flag = True
+
+        while l < r:
+            if s[l].isalnum() == False:
+                l +=1
+
+            elif s[r].isalnum() == False:
+                r -=1
+                
+            else:
+                if s[l].lower() == s[r].lower():
+                
+                    l += 1
+                    r -= 1
+
+                else:
+                    return False
+            
+        return True
