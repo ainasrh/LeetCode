c6f66ad9f2279ce67eae9793cd0ba991e3e342b4
ainas <ainasrhmn@gmail.com>
2025-10-10 20:15:11 +0530

    Time: 3 ms (99.33%), Space: 12.7 MB (78.75%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,22 +1,31 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
 
-        freq = {} 
+        # freq = {} 
 
-        for char in magazine:
-            freq[char] = freq.get(char,0) + 1
+        # for char in magazine:
+        #     freq[char] = freq.get(char,0) + 1
         
-        print(freq)
+        # print(freq)
 
         
-        for char in ransomNote:
-            if char in freq: 
-                freq[char] -= 1
+        # for char in ransomNote:
+        #     if char in freq: 
+        #         freq[char] -= 1
                 
-                if freq[char] == 0:
-                    del freq[char]
-            else:
+        #         if freq[char] == 0:
+        #             del freq[char]
+        #     else:
+        #         return False
+            
+        
+        # return True
+
+        for i in ransomNote:
+            if i not in magazine:
                 return False
+            
+            magazine = magazine.replace(i,"",1)
             
         
         return True
