5ed54a45b7f90b5c1485a63d56de2d476aa11d98
ainas <ainasrhmn@gmail.com>
2025-10-29 15:12:54 +0530

    Time: 4 ms (79.84%), Space: 13.5 MB (81.46%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,19 +1,12 @@
 class Solution(object):
     def moveZeroes(self, nums):
-      
-        # Pointer to keep track of the position for non-zero elements
-        non_zero_index = 0
         
-        # Move all non-zero elements to the beginning of the list
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[non_zero_index] = nums[i]  # Place the non-zero element at the correct index
-                non_zero_index += 1  # Move the index forward for the next non-zero element
+        i = 0
 
-        # Fill the remaining elements with zero
-        for i in range(non_zero_index, len(nums)):
-            nums[i] = 0
-
-    # No need to return anything, list is modified in place.
-
+        for num in nums:
+            if num != 0:
+                nums[i] = num
+                i += 1
         
+        for j in range(i,len(nums)):
+            nums[j] = 0 
