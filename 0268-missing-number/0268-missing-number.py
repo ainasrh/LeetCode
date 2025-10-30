9cbd9955c4ce14bb2b3b64ab691225c561d99f2f
ainas <ainasrhmn@gmail.com>
2025-10-30 15:58:38 +0530

    Time: 2 ms (63.7%), Space: 13.3 MB (63.1%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,15 @@
 class Solution(object):
     def missingNumber(self, nums):
 
-        for i in range(len(nums)+1):
-            if i not in nums:
-                return i
+        # for i in range(len(nums)+1):
+        #     if i not in nums:
+        #         return i
         
+        n = len(nums)
+
+        sum_nums = (n) * (n+1) // 2
+
+        curr = sum(nums)
+        return  sum_nums - curr
+        
+    
