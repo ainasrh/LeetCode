39272cc99e42fc0751e705b2ecf3b2476374c6b4
ainas <ainasrhmn@gmail.com>
2025-10-30 15:12:56 +0530

    Time: 2580 ms (17.63%), Space: 13.3 MB (63.1%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,12 +1,7 @@
 class Solution(object):
     def missingNumber(self, nums):
-        nums_length=len(nums)
-        if 0 not in nums:
-            return 0
-        for i in range(0,nums_length):
-            if nums[i]==0:
-                for j in range(1,nums_length+1):
-                    if j not in nums:
-                        nums[i]=j
-                        return j
-            
+
+        for i in range(len(nums)+1):
+            if i not in nums:
+                return i
+        
